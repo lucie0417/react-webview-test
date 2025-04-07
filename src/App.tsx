cb3a02458c6e1542e53e4958f95130533cb35d8a
lucie0417 <wu.ipei1994@gmail.com>
2025-04-07 11:36:10 +0800

    adjust webview

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import './App.css'
 
 function App() {
   const [messageFromNative, setMessageFromNative] = useState('');
+  const [location, setLocation] = useState<{ latitude: number, longitude: number } | null>(null);
 
   useEffect(() => {
     const handleMessage = (e: MessageEvent) => {
       try {
-        console.log('React收到來自Native', e);
+        alert(`React收到來自Native ${JSON.stringify(e.data)}`);
         const data = JSON.parse(e.data);
-        setMessageFromNative(data);
+        if (data.type === 'UPDATE_LOCATION') {
+          setLocation({
+            latitude: data.latitude,
+            longitude: data.longitude,
+          })
+        }
+
+        setMessageFromNative(JSON.stringify(e.data));
       } catch (error) {
         console.error('Native訊息錯誤', error);
       }
