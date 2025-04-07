63fff047610907cc935f39f2afa92be391bcfd51
lucie0417 <wu.ipei1994@gmail.com>
2025-04-07 10:22:24 +0800

    adjust webview

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [messageFromNative, setMessageFromNative] = useState('');
+
+  useEffect(() => {
+    const handleMessage = (e: MessageEvent) => {
+      try {
+        console.log('React收到來自Native', e);
+      } catch (error) {
+        console.error('解析 Native 訊息錯誤', error);
+      }
+    }
+
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
+
   return (
     <>
-      <div>123456789</div>
+      <div>
+        <h3>React Web嵌入Webview</h3>
+        <p>來自Native的訊息: {messageFromNative}</p>
+
+      </div>
     </>
   )
 }
