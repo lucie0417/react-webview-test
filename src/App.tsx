a4cb6a1fd028b3876f0add36ae5df986b1035e2c
lucie0417 <wu.ipei1994@gmail.com>
2025-04-07 16:59:42 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
     window.addEventListener('message', handleMessage); // Init取得座標
     return () => window.removeEventListener('message', handleMessage);
-  }, []);
+  }, [location]);
 
 
   const sendMsgToNative = () => {
@@ -42,7 +42,7 @@ function App() {
         <p>
           目前位置:
           {location ? (
-            <span> [{location.longitude} , {location.latitude}]</span>
+            <span> [{location?.longitude} , {location?.latitude}]</span>
           ) : '尚未取得位置'}
         </p>
         <button onClick={sendMsgToNative}>向Native框要求位置</button>
