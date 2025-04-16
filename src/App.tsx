3b5816c6c1837752052954db0b312a89669ed962
lucie0417 <wu.ipei1994@gmail.com>
2025-04-16 16:47:54 +0800

    調整框和Web溝通流程

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import './App.css'
 function App() {
   const [location, setLocation] = useState<{ latitude: number, longitude: number } | null>(null);
 
-
   useEffect(() => {
+    const gpsStatus = localStorage.getItem('GPS_ENABLED');
+    if (gpsStatus === 'true') requestInitLocation();
+
     const handleMessage = (e: MessageEvent) => {
       try {
         const data = JSON.parse(e.data);
 
-        if (data.type === 'INIT_LOCATION') {
+        if (data.type === 'LOCATION_INFO') {
           setLocation({
             latitude: data.latitude,
             longitude: data.longitude,
           });
-          alert(`Web Init取得座標 ${JSON.stringify(e.data)}`);
+          alert(`LOCATION_INFO ${JSON.stringify(e.data)}`);
         }
       } catch (error) {
         console.error('Native訊息錯誤', error);
@@ -25,37 +27,48 @@ function App() {
     document.addEventListener('message', handleMessage as EventListener); // For Android 特定系統使用
     return () => window.removeEventListener('message', handleMessage);
   }, []);
+  
 
-
-  const sendMsgToNative = () => {
-    const msg = JSON.stringify({ type: 'UPDATE_LOCATION_FROM_WEB', content: '重取座標~~~' });
-    if ((window as any).ReactNativeWebView) {
-      (window as any).ReactNativeWebView.postMessage(msg);
-    } else {
-      alert('sendMsgToNative Failed!');
-    }
+  // 通知APP取得初始定位
+  const requestInitLocation = () => {
+    const payload = {
+      action: 'GET_INIT_LOCATION',
+      content: '請求進行初始定位!'
+    };
+    (window as any).ReactNativeWebView?.postMessage(JSON.stringify(payload));
   }
 
+  // 重取座標
+  const updateLocation = () => {
+    const payload = {
+      action: 'UPDATE_LOCATION',
+      content: '重取座標!'
+    };
+    (window as any).ReactNativeWebView?.postMessage(JSON.stringify(payload));
+  }
+
+  // 關閉GPS
   const turnOffGPS = () => {
-    const msg = JSON.stringify({ type: 'GPS_OFF_FROM_WEB', content: '關閉GPS!' });
-    if ((window as any).ReactNativeWebView) {
-      (window as any).ReactNativeWebView.postMessage(msg);
-    } else {
-      alert('turnOffGPS Failed!');
-    }
+    const payload = {
+      action: 'GPS_OFF',
+      content: '關閉GPS!'
+    };
+    (window as any).ReactNativeWebView?.postMessage(JSON.stringify(payload));
+
+    localStorage.setItem('GPS_ENABLED', 'false');
   }
 
   return (
     <>
       <div>
-        <h3>我是React Webview~~</h3>
+        <h3>我是React Web~~</h3>
         <p>
           目前位置:
           {location ? (
             <span> [{location?.longitude} , {location?.latitude}]</span>
           ) : '尚未取得位置'}
         </p>
-        <button onClick={sendMsgToNative}>向Native APP框要求位置</button>
+        <button onClick={updateLocation}>向APP框要求位置</button>
         <button onClick={turnOffGPS}>關閉GPS</button>
       </div>
     </>
