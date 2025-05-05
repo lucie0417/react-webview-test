9452a4cf08b908dcdfef3d8de080df94e655916f
Lucie <wu.ipei1994@gmail.com>
2025-05-05 15:17:55 +0800

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom";
 
 
 const Home = () => {
-
     return (
         <div>
             <h1>HOME!</h1>
             <button>
                 <Link to="/gps">GPS!</Link>
             </button>
-            <button>
+            <button style={{ marginLeft: '10px' }}>
                 <Link to="/camera">CAMERA!</Link>
             </button>
         </div >
